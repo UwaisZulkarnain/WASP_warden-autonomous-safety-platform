64a8d11d0d77e13af967a67de7a2b7357ecfcd26
Farkhin <unimazk@gmail.com>
2026-05-30 01:43:22 +0800

    Updated model path to allow seemles running

--- computer_vision/testyolo.py
+++ computer_vision/testyolo.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO(
-    r"C:\Users\User\OneDrive\STUDY\UTM KL\UTM\Project\AI_Showcase\runs\detect\wasp_ppe_v1\weights\best.pt"
+    r"best.pt"
 )
 
 results = model.predict(
